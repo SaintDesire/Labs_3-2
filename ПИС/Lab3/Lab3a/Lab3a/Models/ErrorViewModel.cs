2d69b30b704caa5b7457dc240ea66f14af6a7303
p1v0var <nikitakorshun@mail.ru>
2024-05-11 12:44:07 +0300

    labs update

--- ПИС/Lab3/Lab3a/Lab3a/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab3a.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
